9277b50d840b21e63be63dd6d1453cb2e792baea
Connor <hhcszgd@sina.com>
2019-11-21 17:59:16 +0800

    test danger

---
+++ DDCustomCollectionLayout/Danger.swift
@@ -0,0 +1,21 @@
+//
+//  Danger.swift
+//  DDCustomCollectionLayout
+//
+//  Created by zilly on 2019/11/21.
+//  Copyright © 2019 WY. All rights reserved.
+//
+
+import UIKit
+
+class Danger: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
